f4b7dc546d90b647f32b4962700045a4c2828108
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2015-10-17 02:01:44 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Vladimizr
+
+Super simple [electron](https://github.com/atom/electron) based menubar app
+for transliterating from latin to russian.
+
+![Screenshot](https://raw.github.com/kostia/vladimizr/master/screenshot.png)
+
+## Usage
+
+- Type in the latin text, then press `Shift+Enter`.
+- Press `Escape` to hide the app.
+- Press `Cmd+Q` to quit the app.
+
+## Development
+
+- run `npm install`.
+- run `npm run build` to make Vladimizr.app.
+- run `electron .` to run app from CLI without building Vladimizr.app.
---
+++ vladimizr.js
@@ -0,0 +1,90 @@
+var remote = require('remote');
+
+var map = {
+  "A"   : "А",
+  "a"   : "а",
+  "B"   : "Б",
+  "b"   : "б",
+  "V"   : "В",
+  "v"   : "в",
+  "G"   : "Г",
+  "g"   : "г",
+  "D"   : "Д",
+  "d"   : "д",
+  "E"   : "Е",
+  "e"   : "е",
+  "Jo"  : "Ё",
+  "jo"  : "ё",
+  "Zh"  : "Ж",
+  "zh"  : "ж",
+  "Z"   : "З",
+  "z"   : "з",
+  "I"   : "И",
+  "i"   : "и",
+  "J"   : "Й",
+  "j"   : "й",
+  "K"   : "К",
+  "k"   : "к",
+  "L"   : "Л",
+  "l"   : "л",
+  "M"   : "М",
+  "m"   : "м",
+  "N"   : "Н",
+  "n"   : "н",
+  "O"   : "О",
+  "o"   : "о",
+  "P"   : "П",
+  "p"   : "п",
+  "R"   : "Р",
+  "r"   : "р",
+  "S"   : "С",
+  "s"   : "с",
+  "T"   : "Т",
+  "t"   : "т",
+  "U"   : "У",
+  "u"   : "у",
+  "F"   : "Ф",
+  "f"   : "ф",
+  "Kh"  : "Х",
+  "kh"  : "х",
+  "Ts"  : "Ц",
+  "ts"  : "ц",
+  "Ch"  : "Ч",
+  "ch"  : "ч",
+  "Sh"  : "Ш",
+  "sh"  : "ш",
+  "Sch" : "Щ",
+  "sch" : "щ",
+  "#"   : "ъ",
+  "Y"   : "Ы",
+  "y"   : "ы",
+  "'"   : "ь",
+  "Je"  : "Э",
+  "je"  : "э",
+  "Yu"  : "Ю",
+  "yu"  : "ю",
+  "Ya"  : "Я",
+  "ya"  : "я"
+};
+
+var translit = require('translit')(map);
+
+var currentWindow = remote.getCurrentWindow();
+currentWindow.setResizable(false);
+
+var input = document.getElementById('input');
+input.focus();
+
+document.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13 && event.shiftKey) {
+    var latinText = input.value;
+    russianText = translit(latinText);
+    input.value = russianText;
+    return false;
+  }
+
+  if (event.keyCode === 27) {
+    remote.getCurrentWindow().hide();
+    return false;
+  }
+});
